32907978a3cfb2bb4c0d24c72d38d1bec97398d2
VortexHD <vortexhdwhite@gmail.com>
2021-07-20 16:28:40 +0100

    msm8996-common: Add support for Tethering offload

--- msm8996.mk
+++ msm8996.mk
@@ -362,6 +362,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Tetheroffload
+PRODUCT_PACKAGES += \
+    android.hardware.tetheroffload.config@1.0 \
+    android.hardware.tetheroffload.control@1.0
+	
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
