6fe5a50778f5162c2e66af5b2d0a30e3d5990d8d
thp1997 <tirthpatel49@gmail.com>
2021-09-19 16:52:02 +0530

    msm8996-common: Compile HWUI for Better Performance

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,9 @@ OVERRIDE_QCOM_HARDWARE_VARIANT := msm8996-los
 TARGET_BOOTLOADER_BOARD_NAME := MSM8996
 TARGET_NO_BOOTLOADER := true
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 
